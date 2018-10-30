959e2f238957e6c69482e3d40cfacb55170be396
heights <kiyotomatsuse@gmail.com>
2018-10-30 17:09:37 +0900

    Shader関係のプログラムを作成

---
+++ data/src/client/cpp/render/Shader.cpp
@@ -0,0 +1,101 @@
+#include "./header/shader.h"
+#include "./header/glsl.h"
+#include <stdio.h>
+
+const char *VERTEX_FILE = "./shader/vertexShader.vert";
+const char *FRAGMENT_FILE = "./shader/fragmentShader.frag";
+
+ShaderProgram ShaderManager::shaders = *new ShaderProgram();
+
+/******************************************************************************
+ * コンストラクタ
+******************************************************************************/
+
+/******************************************************************************
+ * ShaderManager
+******************************************************************************/
+bool ShaderManager::initShader()
+{
+    shaders = StaticShaderLoader().generateShader(VERTEX_FILE, FRAGMENT_FILE);
+    return true;
+}
+
+void ShaderManager::startShader()
+{
+    shaders.startShader();
+}
+
+void ShaderManager::stopShader()
+{
+    shaders.stopShader();
+}
+/******************************************************************************
+ * BaseShaderLoader
+******************************************************************************/
+ShaderProgram BaseShaderLoader::generateShader(const char *vertexFilename, const char *fragmentFilename)
+{
+    vertexShaderID = loadShader(vertexFilename, GL_VERTEX_SHADER);
+    fragmentShaderID = loadShader(fragmentFilename, GL_FRAGMENT_SHADER);
+    programID = glCreateProgram();
+    glAttachShader(programID, vertexShaderID);
+    glAttachShader(programID, fragmentShaderID);
+    bindAttributes();
+    glLinkProgram(programID);
+    glValidateProgram(programID);
+    return *new ShaderProgram(programID, vertexShaderID, fragmentShaderID);
+}
+
+GLuint BaseShaderLoader::loadShader(const char *file, int type)
+{
+    GLuint ShaderID = glCreateShader(type);
+
+    //読み込み
+    if (readShaderSource(ShaderID, file))
+    {
+        exit(1);
+    }
+    //コンパイル
+    GLint compiled;
+    glCompileShader(ShaderID);
+    glGetShaderiv(ShaderID, GL_COMPILE_STATUS, &compiled);
+    if (compiled == GL_FALSE)
+    {
+        printf("error compile shader\n");
+    }
+
+    return ShaderID;
+}
+
+void BaseShaderLoader::bindAttribute(int attribute, const char *variableName)
+{
+    glBindAttribLocation(programID, attribute, variableName);
+}
+/******************************************************************************
+ * class StaticShaderLoader
+******************************************************************************/
+void StaticShaderLoader::bindAttributes()
+{
+}
+/******************************************************************************
+ * ShaderProgram
+******************************************************************************/
+ShaderProgram::ShaderProgram()
+{
+    this->programID = 0;
+    this->vertexShaderID = 0;
+    this->fragmentShaderID = 0;
+}
+ShaderProgram::ShaderProgram(GLuint programID, GLuint vertexShaderID, GLuint fragmentShaderID)
+{
+    this->programID = programID;
+    this->vertexShaderID = vertexShaderID;
+    this->fragmentShaderID = fragmentShaderID;
+}
+void ShaderProgram::startShader()
+{
+    glUseProgram(programID);
+}
+void ShaderProgram::stopShader()
+{
+    glUseProgram(0);
+}
---
+++ data/src/client/header/render/shader.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#define GL_GLEXT_PROTOTYPES 1
+
+/******************************************************************************
+ * OBJモデル関係に必要なヘッダーの読み込み
+******************************************************************************/
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <fstream>
+
+/******************************************************************************
+ * class ShaderProgram
+ * 機能1：シェーダIDなどを保持
+ * 機能2：シェーダ開始関数 startShader()
+ * 機能3：シェーダ終了関数 stopShader()
+******************************************************************************/
+class ShaderProgram
+{
+  private:
+    GLuint programID;
+    GLuint vertexShaderID;
+    GLuint fragmentShaderID;
+
+  public:
+    ShaderProgram();
+    ShaderProgram(GLuint programID, GLuint vertexShaderID, GLuint fragmentShaderID);
+    void startShader();
+    void stopShader();
+};
+
+/******************************************************************************
+ * class BaseShaderLoader
+ * 機能1：シェーダプログラムを読み込む
+ * 機能2：シェーダの種類に応じて子クラスが分岐
+******************************************************************************/
+class BaseShaderLoader
+{
+  private:
+    GLuint programID;
+    GLuint vertexShaderID;
+    GLuint fragmentShaderID;
+
+    GLuint loadShader(const char *file, int type);
+    virtual void bindAttributes() = 0;
+
+  public:
+    ShaderProgram generateShader(const char *vertex_filename, const char *fragment_filename);
+
+  protected:
+    void bindAttribute(int attribute, const char *variableName);
+};
+
+/******************************************************************************
+ * class StaticShaderLoader
+ * 機能1：StaticShader用のAttribute変数設定をする
+******************************************************************************/
+class StaticShaderLoader : public BaseShaderLoader
+{
+  private:
+    void bindAttributes();
+
+  public:
+};
+
+/******************************************************************************
+ * class ShaderManager
+ * 機能1：シェーダの初期化
+ * 呼び出しや終了処理の中継
+******************************************************************************/
+class ShaderManager
+{
+  private:
+    static ShaderProgram shaders;
+
+  public:
+    static bool initShader();
+    static void startShader();
+    static void stopShader();
+};

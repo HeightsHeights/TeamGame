201410847ba510217b6fa18646f8c030f8882ed9
heights <kiyotomatsuse@gmail.com>
2018-11-04 17:17:45 +0900

    セッティングファイルを削除

--- data/src/client/cpp/main/mainProgram.cpp
+++ data/src/client/cpp/main/mainProgram.cpp
@@ -11,6 +11,22 @@ int main(int argc, char *argv[])
 {
     char localHostName[] = "localhost";
     char *serverName;
+
+    /* コマンド引数確認 */
+    if (argc == 1)
+    {
+        serverName == localHostName;
+    }
+    else if (argc == 2)
+    {
+        serverName = argv[1];
+    }
+    else
+    {
+        fprintf(stderr, "Usage: %s, Cannot find a Server Name.\n", argv[0]);
+        return -1;
+    }
+
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK))
     {
         fprintf(stderr, "Error --> SDL_Init()\n");

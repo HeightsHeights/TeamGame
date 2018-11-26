cbe6678fc30e39273d45c426715e0274afaf8cde
heights <kiyotomatsuse@gmail.com>
2018-11-26 19:27:01 +0900

    test

--- data/src/client/render/testXLoader/testXLoader.cpp
+++ data/src/client/render/testXLoader/testXLoader.cpp
@@ -1,10 +1,62 @@
 #include "testXLoader.h"
+#include <string.h>
+#include <limits>
 
 #define BUFFER_LENGTH 256
 
+void TestXLoader::skipLine()
+{
+    skip2Key('\n');
+}
+void TestXLoader::skip2Key(const char key)
+{
+    file.ignore(std::numeric_limits<std::streamsize>::max(), file.widen(key));
+}
+void TestXLoader::readMesh()
+{
+}
+void TestXLoader::readFrame(TestXNode *node)
+{
+    char key[BUFFER_LENGTH] = {0};
+
+    file >> key;
+
+    while (1)
+    {
+        if (!(file))
+        {
+            break;
+        }
+        if (0 == strcmp(key, "Header"))
+        {
+            skip2Key('}');
+        }
+        else if (0 == strcmp(key, "template"))
+        {
+            skip2Key('}');
+        }
+        else if (0 == strcmp(key, "Frame"))
+        {
+            std::string frameName;
+            file >> frameName;
+            node->node = new TestXNode();
+            node->node->frameName = frameName;
+        }
+    }
+}
+
+void TestXLoader::readXFile()
+{
+    ret->root.node = NULL;
+    ret->root.next = NULL;
+    ret->root.frameName = "root";
+
+    //保留
+    readFrame(&ret->root);
+}
+
 bool TestXLoader::loadXFile(const char *filePath)
 {
-    char buf[BUFFER_LENGTH] = {0};
 
     file.open(filePath, std::fstream::in);
     if (!file.is_open())
@@ -12,6 +64,11 @@ bool TestXLoader::loadXFile(const char *filePath)
         fprintf(stderr, "Error --> xFile is not found\n");
         return false;
     }
+
+    readXFile();
+
+    file.close();
+    return true;
 }
 
 TestXModel *TestXLoader::load(const std::string dirPath, const std::string fileName)
--- data/src/client/render/testXLoader/testXLoader.h
+++ data/src/client/render/testXLoader/testXLoader.h
@@ -1,10 +1,27 @@
 #include "testXModel.h"
+#include <vector>
+#include "../../../common/math/vector/vectorSet.h"
 
 class TestXLoader
 {
 private:
   TestXModel *ret;
   std::ifstream file;
+
+  std::vector<Vector3f> vertices;
+  std::vector<Vector2f> textures;
+  std::vector<Vector3f> normals;
+
+  std::vector<unsigned int> vertexIndices;
+  std::vector<unsigned int> textureIndices;
+  std::vector<unsigned int> normalIndices;
+
+  void skipLine();
+  void skip2Key(const char key);
+
+  void readMesh();
+  void readFrame(TestXNode *node);
+  void readXFile();
 
   bool loadXFile(const char *filePath);
 

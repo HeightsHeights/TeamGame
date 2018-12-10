38a865254d5775b06cf59a8e53fbb414d9257a60
pocky64 <kasoucoin39@gmail.com>
2018-12-10 16:56:28 +0900

    audio

--- data/src/client/audio/audioManager.cpp
+++ data/src/client/audio/audioManager.cpp
@@ -12,7 +12,7 @@ std::string SE_NAME[SE_NUMBER] =
         "",
 };
 
-// bool AudioManager::initAudio(){
+// bool AudioManager::init(){
 //     Mix_Init(MIX_INIT_MP3);
 //     if(Mix_OpenAudio(MIX_DEFAULT_FREQUENCY,MIX_DEFAULT_FORMAT,2,1024) == -1){
 //         fprintf(stderr,"Error --> open audio device\n");
@@ -21,10 +21,3 @@ std::string SE_NAME[SE_NUMBER] =
 //     return true;
 // }
 
-// void AudioManager::playAudio(Sound_ID aid){
-//     audio[aid] -> playAudio();
-// }
-
-// void AudioManager::stopAudio(Sound_ID aid){
-//     audio[aid] -> stopAudio();
-// }

43b56777b6cfc3105590f8de4cd6b6ad1dc91b27
heights <kiyotomatsuse@gmail.com>
2019-01-09 21:34:02 +0900

    commit

--- data/src/client/scene/sceneMainGame.cpp
+++ data/src/client/scene/sceneMainGame.cpp
@@ -16,13 +16,13 @@ bool SceneMainGame::init()
     tile = ObjModelLoader().load("./data/res/gui/obj/", "tile");
     mush = ObjModelLoader().load("./data/res/gui/obj/", "mush");
 
-    std::string IMAGE_NAME = "numbers/number_";
+    std::string numbersNameTemplate = "numbers/number_";
     for (int i = 0; i < 10; i++)
     {
         char numberString[4];
         sprintf(numberString, "%d", i);
 
-        std::string imageName = IMAGE_NAME + std::string(numberString) + PNG_FILE_EXTENSION;
+        std::string imageName = numbersNameTemplate + std::string(numberString) + PNG_FILE_EXTENSION;
         imageNumber[i] = GuiImageLoader().load((PNG_DIR_PATH + imageName).c_str());
     }
 

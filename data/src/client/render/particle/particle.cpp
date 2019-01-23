b0df407823bb1ccb03f0b72d25f2fb006a8742e8
heights <kiyotomatsuse@gmail.com>
2019-01-23 19:25:27 +0900

    commit

---
+++ data/src/client/render/particle/particle.cpp
@@ -0,0 +1,104 @@
+#include "./particle.h"
+
+#define BUFFER_OFFSET(bytes) ((GLubyte *)NULL + (bytes))
+
+Particle::Particle() {}
+Particle::Particle(GLuint texId)
+{
+    this->texId = texId;
+}
+void Particle::setSpriteNum(Touple2f spriteMaxNum)
+{
+    this->spriteMaxNum = spriteMaxNum;
+}
+
+void Particle::bindVao()
+{
+    glBindVertexArray(vao);
+}
+void Particle::unbindVao()
+{
+    glBindVertexArray(0);
+}
+
+void Particle::bindTexture()
+{
+    glBindTexture(GL_TEXTURE_2D, texId);
+}
+void Particle::unbindTexture()
+{
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+GLuint Particle::createArrayBuffer()
+{
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vboVertex);
+    return vbo;
+}
+void Particle::storeData()
+{
+    std::function<int(int, int)> plus = [](int a, int b) { return a + b; };
+    plus(1, 1);
+
+    const GLuint indexData[] = {
+        0, 2, 3,
+        0, 3, 1};
+    const GLfloat vertexData[] = {
+        -1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 0.0f,
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f};
+    const GLfloat uvData[] = {
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        0.0f, 1.0f,
+        1.0f, 1.0f};
+
+    glGenVertexArrays(1, &vao);
+    bindVao();
+    glGenBuffers(1, &vboIndex);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboIndex);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexData), indexData, GL_STATIC_DRAW);
+
+    vboVertex = createArrayBuffer();
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
+
+    vboUv = createArrayBuffer();
+    glBufferData(GL_ARRAY_BUFFER, sizeof(uvData), uvData, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void *)0);
+
+    vboPos = createArrayBuffer();
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(pData), (void *)0);
+
+    glVertexAttribDivisor(0, 0);
+    glVertexAttribDivisor(1, 0);
+    glVertexAttribDivisor(2, 1);
+    unbindVao();
+}
+
+void Particle::generate(unsigned int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        PData object = PData();
+        object.pos = this->emitterPos;
+        pData.push_back(object);
+    }
+}
+void Particle::draw()
+{
+    bindVao();
+
+    // glBindBuffer(GL_ARRAY_BUFFER, posBufferObject);
+    // glBufferSubData(GL_ARRAY_BUFFER, 0, R * 3 * sizeof(GLfloat), position_data);
+    // glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    // glBufferSubData(GL_ARRAY_BUFFER, 0, R * 3 * sizeof(GLfloat), g_color_buffer_data);
+    glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void *)0, pData.size());
+    unbindVao();
+}
---
+++ data/src/client/render/particle/particle.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <functional>
+#include <vector>
+#define GL_GLEXT_PROTOTYPES 1
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include "./pData.h"
+#include "../../../common/math/touple/touple2f.h"
+#include "../../../common/math/vector/vector3f.h"
+
+class Particle
+{
+private:
+  Vector3f emitterPos;
+
+  GLuint vao;
+  GLuint texId;
+
+  GLuint vboIndex;
+  GLuint vboVertex;
+  GLuint vboUv;
+  GLuint vboPos;
+
+  Touple2f spriteMaxNum;
+
+  std::vector<PData> pData;
+
+  void storeData();
+
+  GLuint createArrayBuffer();
+
+  void bindVao();
+  void unbindVao();
+
+  void bindTexture();
+  void unbindTexture();
+
+  void upDate();
+
+public:
+  Particle();
+  Particle(GLuint texId);
+  void generate(unsigned int num);
+  void draw();
+
+  void setSpriteNum(Touple2f spriteMaxNum);
+};

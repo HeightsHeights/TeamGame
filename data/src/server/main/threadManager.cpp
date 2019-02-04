2bc311c343ef7b192a5422cd692a563e49755d5d
heights <kiyotomatsuse@gmail.com>
2019-02-04 13:55:51 +0900

    commit

--- data/src/server/main/threadManager.cpp
+++ data/src/server/main/threadManager.cpp
@@ -29,7 +29,7 @@ int ThreadManager::dataProcessingThread(void *data)
         {
             SDL_AtomicDecRef((SDL_atomic_t *)data);
         }
-        SDL_Delay(5);
+        SDL_Delay(20);
     }
     return 1;
 }
